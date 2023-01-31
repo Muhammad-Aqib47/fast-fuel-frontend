c8052d13aeacf9c37d66342ae955724881393af6
Saleem Ahmad <saleemuskan@gmail.com>
2023-01-31 15:05:16 +0500

    connected with backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
 import './App.css';
+import BuyerLoginForm from './components/buyer-login/buyer-login';
+import BuyerSignUpForm from './components/buyer-signup/buyer-signup';
+import SellerLoginForm from './components/seller-login/seller-login';
+import SellerSignUpForm from './components/seller-signup/seller-signup'; 
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      
-      <h1>hello</h1>
-    </div>
-  );
-}
+    <>
+       <Routes>
+          <Route path="/sellerlogin" element={<SellerLoginForm/>} />
+          <Route path="/sellersignup" element={<SellerSignUpForm />} />
+          <Route path="/buyerlogin" element={<BuyerLoginForm />} />
+          <Route path="/buyersignup" element={<BuyerSignUpForm />} />
+
+       </Routes>
+    </>
+ );
+};
+
 
 export default App;
---
+++ src/components/buyer-login/buyer-login.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
+import "./login-signup.css";
+import { useState } from "react";
+
+const BuyerLoginForm = () => {
+  const [apiResponse, setApiResponse] = useState(null);
+
+  const handleSubmit = async (values, actions,e) => {
+
+    console.log(">>>>>>>>>>>>>>>>>>>>", values);
+
+    try {
+      const response = await fetch("http://localhost:3002/api/buyers/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      console.log(".>>>><<<>>>>", data);
+      setApiResponse(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      actions.setSubmitting(false);
+    }
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Email must be a valid email")
+      .required("Email cannot be blank."),
+    password: Yup.string().required("Password is required"),
+    // .min(8, "Password must have at least 8 characters").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]*$/, "Password must have at least one uppercase letter, one lowercase letter, and one digit")
+  });
+
+  return (
+    <div className="main">
+      <div className="form-container">
+        <h2 className="title-text">Buyer Login</h2>
+        <div className="image-container">
+          <img
+            className="profile-image"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTPzBF74hA1avhZGbDvPRJK-cAaN7KL9I8ew&usqp=CAU"
+            alt="profile"
+          />
+        </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+
+          // onSubmit={(values) => {
+          //   console.log(values);
+          // }}
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form>
+              <div className="form">
+                <label htmlFor="email" className="input-fields-lable">
+                  Email
+                </label>
+                <Field
+                  className="input-fields"
+                  name="email"
+                  placeholder="Email"
+                />
+                {errors.email && touched.email ? (
+                  <div className="error-meessage">{errors.email}</div>
+                ) : null}
+
+                <label htmlFor="password" className="input-fields-lable">
+                  Password
+                </label>
+                <Field
+                  className="input-fields"
+                  name="password"
+                  placeholder="Password"
+                  type="password"
+                />
+                {errors.password && touched.password ? (
+                  <div className="error-meessage">{errors.password}</div>
+                ) : null}
+
+                <button
+                  className="submit-buton"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Login
+                </button>
+                {apiResponse && <p>{apiResponse.message}</p>}
+              </div>
+            </Form>
+          )}
+        </Formik>
+        <p className="text-before-link">
+          Don't have account?{" "}
+          <Link className="link-to-redirect" to="/buyersignup">
+            Signup
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default BuyerLoginForm;
