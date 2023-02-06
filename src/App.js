d18645aa7a40618af4ae0d34c7617862946e4e58
unknown <saadayub81@gmail.com>
2023-02-06 15:13:15 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/landing-page/navbar';
-import Banner from './components/landing-page/banner';
-import Video from './components/landing-page/how it works';
-import ServicesCard from './components/landing-page/services-card';
-import Faqs from './components/landing-page/faqs';
-import ServeAreas from './components/landing-page/where we serve';
-import Footer from './components/landing-page/footer';
-import Contact from './components/landing-page/contact-us';
+import LandingPage from './components/landing-page';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Banner/>
-      <Video/>
-      <ServicesCard />
-      <Faqs/>
-      <ServeAreas/>
-      <Contact/>
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
+  
      
     </div>
   );
